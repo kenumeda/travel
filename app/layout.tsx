6b64493112f2b06f32797e7bdf8e1d13c8cec3dd
Ken Umeda <kenumeda1@gmail.com>
2023-05-15 16:36:33 +1000

    changed the layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,25 @@
+import Head from "next/head";
+import React from "react";
+
+export const metadata = {
+  title: "Travel",
+  description: "Next Travel Website",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <>
+      <Head>
+        <title>{metadata.title}</title>
+        <meta name="description" content={metadata.description} />
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      {children}
+    </>
+  );
+}
